0c0cadbd7a18763aeed87a904b7c38af9e88d8f7
Anders Sildnes <andsild@posteo.net>
2021-03-21 09:44:29 +0100

    Time and tide wait for no man.

--- frontend/qwdeutil/Test/TestMain.hs
+++ frontend/qwdeutil/Test/TestMain.hs
@@ -7,6 +7,24 @@ import Test.HUnit
 import Test.HUnit.Text
 import System.Exit (exitFailure)
 
+-- | Convenience wrapper for 'runTestTT'.
+-- --   Simply runs 'runTestTT' and then exits back to the OS,
+-- --   using 'exitSuccess' if there were no errors or failures,
+-- --   or 'exitFailure' if there were. For example:
+-- --
+-- --   > tests :: Test
+-- --   > tests = ...
+-- --   >
+-- --   > main :: IO ()
+-- --   > main = runTestTTAndExit tests
+--
+runTestTTAndExit :: Test -> IO ()
+runTestTTAndExit tests = do
+  c <- runTestTT tests
+  if (errors c == 0) && (failures c == 0)
+    then exitSuccess
+    else exitFailure
+
 main :: IO ()
 main = do
   -- runTest[T]ext(to)[T]erminal
